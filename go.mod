2982a0b821098e4a13378591a5d2ae001e1c7814
bsyrlhabibi <bsyrlhabibi@gmail.com>
2023-08-02 17:23:07 +0700

    app belajar module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bsyrlhabibi/app-belajar-module
+
+go 1.20
+
+require github.com/bsyrlhabibi/go-belajar-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_belajar_module "github.com/bsyrlhabibi/go-belajar-module/v2"
+)
+
+func main() {
+	fmt.Println(go_belajar_module.SayHello("Bibi"))
+}
